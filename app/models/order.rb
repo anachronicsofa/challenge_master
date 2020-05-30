818e6fe4d1fd6c1fbe4950ad152509b0e5bf9ba1
aline r <alineramosv48@gmail.com>
2020-05-30 02:57:59 -0300

    model order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ApplicationRecord
+  validates :reference, presence: true
+  validates :client_name, presence: true
+  validates :purchase_channel, presence: true
+  validates :delivery_service, presence: true
+  validates :status, presence: true
   belongs_to :batch
 end
