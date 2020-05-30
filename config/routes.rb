5fa07d2b3fa6a6f8d3edd91508980cf94a097204
aline r <alineramosv48@gmail.com>
2020-05-30 01:24:21 -0300

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :batches
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      resources :orders
+      resources :batches
+    end
+  end
 end
