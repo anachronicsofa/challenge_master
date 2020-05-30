b6b1e783d0c2b6de6ea1364356f75fac49854ce5
aline r <alineramosv48@gmail.com>
2020-05-30 01:14:25 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+  Batch.create!(
+        reference: "Lote #{i}", 
+        purchase_channel: Faker::Alphanumeric.alpha(number: 10)
+      )
+end
+
+10.times do |i|
+  Order.create!(
+        reference: "Pedido #{i}",
+        client_name: Faker::Movies::HitchhikersGuideToTheGalaxy.character,
+        purchase_channel: Faker::Alphanumeric.alpha(number: 10),
+        address: Faker::Movies::HitchhikersGuideToTheGalaxy.location,
+        delivery_service: Faker::Alphanumeric.alpha(number: 10),
+        total_value: Faker::Number.number(digits: 4),
+        line_items: Faker::Alphanumeric.alpha(number: 10),
+        status: %w[ready production closing sent].sample,
+        batch: Batch.all.sample
+      )
+end
