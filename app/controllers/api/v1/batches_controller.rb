66e92be53a74cd1a4bae083e06a0279420e77958
aline r <alineramosv48@gmail.com>
2020-06-04 13:12:08 -0300

    Create Batch method updated

--- app/controllers/api/v1/batches_controller.rb
+++ app/controllers/api/v1/batches_controller.rb
@@ -1,59 +1,72 @@
-require_relative "orders_controller.rb"
+require_relative 'orders_controller.rb'
 
 module Api
   module V1
     class BatchesController < ApplicationController 
+      before_action :set_batch, only: %i[show printing_done deliver_order]
       def index
-        @batch = Batch.order('created_at DESC');
+        @batch = Batch.order('created_at DESC')
         render json: @batch
       end
 
       def create
         @batch = Batch.new(batch_params)
         if @batch.save
-          render json: @batch
+          add_order
         else
           render json: @batch.errors
         end
       end
 
-      def add_order(orders)
-        batch.n_orders = 0
-        orders.each do |order| 
-          if (order.purchase_channel==batch.purchase_channel && order.status=="ready")
-            batch.orders << order    
-            order.batch.id = @batch.id
-            order.status = 'production'
-            n_orders +=1
-          else
-            erros=[]
-            erros.push(order)
-          end
-        end
-        if erros.count != 0 
-          puts "ERROR! Cannot add this orders: #{erros}"
-        end
-      puts "Number of Orders: #{n_orders}"
-      render json: batch.orders
-    end
+      def show
+        @batch = Batch.find(params[:id])
+        render json: @batch
+      end
 
       def printing_done
         @batch.orders.status = 'closing'
       end
 
-      def delivery_order(delivery_service)
-        @batch.orders.each do |order|
-          if order.delivery_service == delivery_service
-            order.status = 'sent'
+      def deliver_order
+        if params[:delivery_service].blank?
+          render json: { error: 'Empty delivery_service' }, status: 404
+        else
+          @batch.orders.each do |order|
+            if order.delivery_service == params[:delivery_service]
+              order.status = 'sent'
+            end
           end
         end
       end
 
       private
 
+      def set_batch
+        @batch = Batch.find(params[:id])
+      end
+
       def batch_params
         params.permit(:reference, :purchase_channel)
       end
+
+      def add_order
+        erros = []
+        orders = Order.find(params[:order_id])
+        orders.each do |order|
+          if order.purchase_channel == @batch.purchase_channel && order.status == 'ready'
+            @batch.orders << order
+            order.batch.id = @batch.id
+            order.status = 'production'
+          else
+            erros.push(order)
+          end
+        end
+        if erros.count != 0
+          render json: { status: 'success', message: "#{@batch.orders.count} orders added to the batch. #{@batch.orders}./n ERROR: #{erros} can't be add (differente purchase channel." }
+        else
+          render json: { status: 'success', message: "#{@batch.orders.count} orders added to the batch. #{@batch.orders}" }
+        end
+      end
     end
-  end 
+  end
 end
