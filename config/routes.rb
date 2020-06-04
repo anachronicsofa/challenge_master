26aac906d07382704536fe883b25e7eb9a24d879
aline r <alineramosv48@gmail.com>
2020-06-04 13:12:53 -0300

    New route add

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :orders
-      resources :batches
+      resources :orders do
+        collection do
+          get :show_status_by_name
+          get :show_closing_order_by_purchase_ch
+        end
+      end
+      resources :batches do
+        member do
+          post :printing_done
+          post :deliver_order
+          post :create
+        end
+      end
     end
   end
 end
